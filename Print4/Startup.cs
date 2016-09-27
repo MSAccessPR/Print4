070a07c4ca454149b54fd3d19736cb4f8aae6bab
JoseCardona0 <jcardona@IT05>
2016-09-27 16:01:50 -0400

    Add project files.

---
+++ Print4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Print4.Startup))]
+namespace Print4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
